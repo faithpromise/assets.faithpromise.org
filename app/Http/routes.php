a55bfb38178ef6c569ba70515420548fa071120f
Brad Roberts <brad@bradroberts.net>
2015-07-22 17:54:07 -0400

    Added a route that resizes images

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,57 @@
 |
 */
 
+use Illuminate\Http\Response;
+use Intervention\Image\ImageManagerStatic as Image;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{display_size}/{image_size}/{image_path}', function ($display_size, $image_size, $image_path) {
+
+    $pixel_density = 2;
+
+    $display_sizes = [
+        'sm' => 320,
+        'md' => 480,
+        'lg' => 600,
+        'xl' => 960
+    ];
+
+    $image_sizes = [
+        'full'    => 1,
+        'half'    => .5,
+        'third'   => .34,
+        'quarter' => .25
+    ];
+
+    $compressions = [
+        'sm' => 80,
+        'md' => 80,
+        'lg' => 80,
+        'xl' => 80
+    ];
+
+    $src_path = public_path($image_path);
+    $new_width = (int)(($display_sizes[$display_size] * $pixel_density) * $image_sizes[$image_size]);
+    $compression = $compressions[$display_size];
+
+    // Throw 404 if image does not exist
+    if (! file_exists($src_path)) {
+        return response('image not found', 404);
+    }
+
+    $img = Image::make($src_path)->resize($new_width, null, function($constraint) {
+        $constraint->aspectRatio();
+        $constraint->upsize();
+    });
+
+    $response = new Response($img->encode(null, $compression), 200);
+    $response->header('Content-Type', $img->mime());
+    $response->header('Cache-Control', 'max-age=31536000');
+
+    return $response;
+
+})->where('display_size', '(sm|md|lg|xl)')->where('image_size', '(full|half|third|quarter)')->where('image_path', '.*');
